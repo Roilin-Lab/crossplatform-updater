48b3a64d15e03d1ba6acab8db4d9065add2b63a8
Roilin-Lab <skirills267@gmail.com>
2025-09-15 13:09:53 +0300

    init

---
+++ src/main/java/io/github/roilin/crossplatform_updater/CrossplatformUpdaterApplication.java
@@ -0,0 +1,14 @@
+package io.github.roilin.crossplatform_updater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossplatformUpdaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrossplatformUpdaterApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
---
+++ src/test/java/io/github/roilin/crossplatform_updater/CrossplatformUpdaterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.roilin.crossplatform_updater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossplatformUpdaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
