b1b3cecbca6fd139d9d5ee614aa4f9eda396210e
Roilin-Lab <skirills267@gmail.com>
2025-09-29 11:29:45 +0300

    relate user device to user

--- src/main/java/io/github/roilin/crossplatform_updater/models/user/User.java
+++ src/main/java/io/github/roilin/crossplatform_updater/models/user/User.java
@@ -1,9 +1,19 @@
 package io.github.roilin.crossplatform_updater.models.user;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import io.github.roilin.crossplatform_updater.models.UserDevice;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
@@ -19,9 +29,19 @@ public class User {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
   @Email
+  @Column(nullable = false, unique = true)
   private String email;
+
+  @Column(nullable = false, unique = true)
   private String username;
+
+  @Column(nullable = false)
   private String password;
   private boolean enabled;
+
+  @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+  @JsonIgnore
+  private Set<UserDevice> devices = new HashSet<>();
 }
