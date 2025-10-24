5d71bb2fd2e3ce9911d86c474cd856b94f54e568
roilin <skirills267@gmail.com>
2025-10-24 11:06:45 +0300

    feat: add jwt auth filter

---
+++ src/main/java/io/github/roilin/crossplatform_updater/jwt/JwtAuthFilter.java
@@ -0,0 +1,58 @@
+package io.github.roilin.crossplatform_updater.jwt;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private JwtTokenProvider tokenProvider;
+    private UserDetailsService userDetailsService;
+
+    @Value("${jwt.access.cookieName}")
+    private String accessTokenName;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String token = "";
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (accessTokenName.equals(cookie.getName())) {
+                token = cookie.getValue();
+                break;
+            }
+        }
+        if ("".equals(token) || !tokenProvider.isValid(token)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        String username = tokenProvider.getUsername(token);
+        if (username == null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null,
+                userDetails.getAuthorities());
+        authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+        filterChain.doFilter(request, response);
+    }
+}
