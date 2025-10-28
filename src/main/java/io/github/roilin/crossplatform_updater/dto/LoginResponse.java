ca21d4f80665128c4c9a08edf4b6223533e27e5b
roilin <skirills267@gmail.com>
2025-10-28 09:40:42 +0300

    refactor: change LoginResponse record - the answer must not contain a password

--- src/main/java/io/github/roilin/crossplatform_updater/dto/LoginResponse.java
+++ src/main/java/io/github/roilin/crossplatform_updater/dto/LoginResponse.java
@@ -1,12 +1,9 @@
 package io.github.roilin.crossplatform_updater.dto;
 
 import java.io.Serializable;
-import java.util.Set;
 
 public record LoginResponse(
-    Long id,
+    boolean isLogged,
     String username,
-    String password,
-    String role,
-    Set<String> permissions
+    String role
 ) implements Serializable {}
