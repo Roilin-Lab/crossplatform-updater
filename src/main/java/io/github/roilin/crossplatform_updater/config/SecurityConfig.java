114d6b0dd47c295919870bd0fa04f60ea7bf014f
roilin <skirills267@gmail.com>
2025-10-31 23:18:21 +0300

    feat: add filter chain in security config

--- src/main/java/io/github/roilin/crossplatform_updater/config/SecurityConfig.java
+++ src/main/java/io/github/roilin/crossplatform_updater/config/SecurityConfig.java
@@ -1,12 +1,24 @@
 package io.github.roilin.crossplatform_updater.config;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 
+import io.github.roilin.crossplatform_updater.jwt.JwtAuthEntryPoint;
+import io.github.roilin.crossplatform_updater.jwt.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
@@ -14,6 +26,47 @@ import lombok.RequiredArgsConstructor;
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+    private final JwtAuthEntryPoint jwtAuthEntryPoint;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.cors(cors -> cors.configurationSource(request -> {
+            CorsConfiguration configuration = new CorsConfiguration();
+            configuration.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
+            configuration.setAllowedMethods(Collections.singletonList("*"));
+            configuration.setAllowCredentials(true);
+            configuration.setAllowedHeaders(Collections.singletonList("*"));
+            configuration.setMaxAge(3600L);
+            return configuration;
+        }));
+        http.authorizeHttpRequests(authorize -> {
+            authorize.requestMatchers("/swagger-ui/**").permitAll();
+            authorize.requestMatchers("/v3/api-docs/**").permitAll();
+            authorize.requestMatchers("/api/auth/login", "/login").permitAll();
+            // authorize.anyRequest().permitAll();
+            authorize.anyRequest().authenticated();
+        });
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.exceptionHandling(exception -> exception.authenticationEntryPoint(jwtAuthEntryPoint));
+        http
+                .formLogin(form -> form
+                        .permitAll())
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .permitAll());
+        http
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
 
     @Bean
     public static PasswordEncoder passwordEncoder() {
