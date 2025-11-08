ba27513bfa7e4a6b31e96f7d7069eac2466c6182
roilin <skirills267@gmail.com>
2025-11-08 09:10:17 +0300

    feat: add user to userLoggedDto map

--- src/main/java/io/github/roilin/crossplatform_updater/mapper/UserMapper.java
+++ src/main/java/io/github/roilin/crossplatform_updater/mapper/UserMapper.java
@@ -3,17 +3,24 @@ package io.github.roilin.crossplatform_updater.mapper;
 import java.util.stream.Collectors;
 
 import io.github.roilin.crossplatform_updater.dto.UserDto;
+import io.github.roilin.crossplatform_updater.dto.UserLoggedDto;
 import io.github.roilin.crossplatform_updater.models.user.Permission;
 import io.github.roilin.crossplatform_updater.models.user.User;
 
 public class UserMapper {
     public static UserDto toDto(User user) {
         return new UserDto(
-            user.getId(),
-            user.getUsername(),
-            user.getPassword(),
-            user.getRole().getAuthority(),
-            user.getRole().getPermissions().stream().map(Permission::getAuthority).collect(Collectors.toSet())
-        );
+                user.getId(),
+                user.getUsername(),
+                user.getPassword(),
+                user.getRole().getAuthority(),
+                user.getRole().getPermissions().stream().map(Permission::getAuthority).collect(Collectors.toSet()));
+    }
+
+    public static UserLoggedDto toUserLoggedDto(User user) {
+        return new UserLoggedDto(
+                user.getUsername(),
+                user.getRole().getAuthority(),
+                user.getRole().getPermissions().stream().map(Permission::getAuthority).collect(Collectors.toSet()));
     }
 }
