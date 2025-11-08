2a698293094633abd141f0cb9995862e580c5d4b
roilin <skirills267@gmail.com>
2025-11-08 09:18:33 +0300

    feat: add refresh, logout, info routes

--- src/main/java/io/github/roilin/crossplatform_updater/controllers/AuthController.java
+++ src/main/java/io/github/roilin/crossplatform_updater/controllers/AuthController.java
@@ -1,18 +1,19 @@
 package io.github.roilin.crossplatform_updater.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.github.roilin.crossplatform_updater.dto.LoginRequest;
 import io.github.roilin.crossplatform_updater.dto.LoginResponse;
-import io.github.roilin.crossplatform_updater.services.impl.AuthenticationService;
+import io.github.roilin.crossplatform_updater.dto.UserLoggedDto;
+import io.github.roilin.crossplatform_updater.services.AuthenticationService;
 import lombok.RequiredArgsConstructor;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
 
 @RestController
 @RequestMapping("/api/auth")
@@ -22,10 +23,28 @@ public class AuthController {
 
   @PostMapping("/login")
   public ResponseEntity<LoginResponse> login(
-    @CookieValue(name = "access-token", required = false) String accessToken,
-    @CookieValue(name = "refresh-token", required = false) String refreshToken,
-    @RequestBody LoginRequest loginRequest
-  ) {
+      @CookieValue(name = "access-token", required = false) String accessToken,
+      @CookieValue(name = "refresh-token", required = false) String refreshToken,
+      @RequestBody LoginRequest loginRequest) {
     return authenticationService.login(loginRequest, accessToken, refreshToken);
+  }
+
+  @PostMapping("/refresh")
+  public ResponseEntity<LoginResponse> refresh(
+      @CookieValue(name = "refresh-token", required = false) String refreshToken) {
+    return authenticationService.refresh(refreshToken);
+  }
+
+  @PostMapping("/logout")
+  public ResponseEntity<LoginResponse> logout(
+      @CookieValue(name = "access-token", required = false) String accessToken,
+      @CookieValue(name = "refresh-token", required = false) String refreshToken) {
+    return authenticationService.logout(accessToken, refreshToken);
+  }
+
+  @PreAuthorize("isAuthenticated()")
+  @GetMapping("/info")
+  public ResponseEntity<UserLoggedDto> userLoggedInfo() {
+    return ResponseEntity.ok(authenticationService.info());
   }
 }
