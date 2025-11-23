160855a5f8078937469d879492644b120a65ee34
roilin <skirills267@gmail.com>
2025-11-23 23:12:12 +0300

    feat: add get methods by properties

--- src/main/java/io/github/roilin/crossplatform_updater/repositories/ApplicationRepository.java
+++ src/main/java/io/github/roilin/crossplatform_updater/repositories/ApplicationRepository.java
@@ -1,11 +1,18 @@
 package io.github.roilin.crossplatform_updater.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import io.github.roilin.crossplatform_updater.models.Application;
+import java.util.List;
+import io.github.roilin.crossplatform_updater.models.enums.ApplicationType;
 
 @Repository
-public interface ApplicationRepository extends JpaRepository<Application, Long> {
+public interface ApplicationRepository extends JpaRepository<Application, Long>, JpaSpecificationExecutor<Application> {
+  List<Application> findByDeveloper(String developer);
 
+  List<Application> findByPublisher(String publisher);
+
+  List<Application> findByType(ApplicationType type);
 }
