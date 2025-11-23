dd6f3de06f3a18ea1bb7c3d6b02a3b280fe31b46
roilin <skirills267@gmail.com>
2025-11-23 14:25:49 +0300

    refactor: delete unused method

--- src/main/java/io/github/roilin/crossplatform_updater/repositories/UserDeviceRepository.java
+++ src/main/java/io/github/roilin/crossplatform_updater/repositories/UserDeviceRepository.java
@@ -1,17 +1,14 @@
 package io.github.roilin.crossplatform_updater.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import io.github.roilin.crossplatform_updater.models.AppVersion;
 import io.github.roilin.crossplatform_updater.models.UserDevice;
 import io.github.roilin.crossplatform_updater.models.user.User;
-
-import java.util.List;
 
 @Repository
 public interface UserDeviceRepository extends JpaRepository<UserDevice, Long> {
   List<UserDevice> findAllByUser(User user);
-
-  List<UserDevice> findAllByVersion(AppVersion version);
 }
